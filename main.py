886f055f3517196a02ed3d6065f7bb7a21005230
David Murillo <dmz866@hotmail.com>
2025-02-20 18:26:16 -0500

    audio to text and text to audio

--- main.py
+++ main.py
@@ -7,6 +7,34 @@ import webbrowser
 import datetime
 import wikipedia
 
+# listen to microphone and returns audio text
+def audio_to_text():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as origin:
+        r.pause_threshold = 0.8
+        print('Start talking')
+        audio = r.listen(origin)
+
+        try:
+            #search on google
+            request = r.recognize_google(audio, language='en-US')
+            print(f"You Said: {request}")
+
+            return request
+
+        except sr.UnknownValueError:
+            print("Error!")
+
+            return "Waiting...."
 
 
+def text_to_audio(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+
+t = audio_to_text()
+text_to_audio(t)
 
