4a3d16f87280a57677b4635c8165e438c6b48529
David Murillo <dmz866@hotmail.com>
2025-02-20 20:51:08 -0500

    voice list

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import webbrowser
 import datetime
 import wikipedia
 
+id1= 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0'
+id2= 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0'
+
+
 # listen to microphone and returns audio text
 def audio_to_text():
     r = sr.Recognizer()
@@ -29,12 +33,18 @@ def audio_to_text():
             return "Waiting...."
 
 
-def text_to_audio(text):
+def text_to_audio(text, id):
     engine = pyttsx3.init()
+    engine.setProperty('voice', id)
+    #for voice in engine.getProperty('voices'):
+    #    print(voice)
+
     engine.say(text)
     engine.runAndWait()
 
 
-t = audio_to_text()
-text_to_audio(t)
+#t = audio_to_text()
+#text_to_audio(t)
+text_to_audio("Hello", id1)
+text_to_audio("Hello", id2)
 
